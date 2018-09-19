ee576ec5a21e9ce10616d72aa9ed07188d825357
zhuyali <495162889@qq.com>
2018-09-19 20:51:54 +0800

    add buttons

--- src/container/InterfaceDetail.vue
+++ src/container/InterfaceDetail.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="g-interface-detail">
-    <el-breadcrumb class="m-breadcrumb" separator-class="el-icon-arrow-right">
-      <el-breadcrumb-item><a href="/">所有项目</a></el-breadcrumb-item>
-      <el-breadcrumb-item>{{ projectName }}</el-breadcrumb-item>
-      <el-breadcrumb-item v-if="breadcrumb">{{ breadcrumb }}</el-breadcrumb-item>
-    </el-breadcrumb>
+    <div class="g-interface-header">
+      <el-breadcrumb class="m-breadcrumb" separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item><a href="/">所有项目</a></el-breadcrumb-item>
+        <el-breadcrumb-item>{{ projectName }}</el-breadcrumb-item>
+        <el-breadcrumb-item v-if="breadcrumb">{{ breadcrumb }}</el-breadcrumb-item>
+      </el-breadcrumb>
+      <div class="m-btngroup">
+        <div class="u-switch" type="primary" size="mini">
+          <el-switch 
+            v-model="enabled"
+            active-color="#13ce66"
+            :active-value="true"
+            :inactive-value="false">
+          </el-switch>
+          开启全局代理
+        </div>
+        <el-button type="primary" size="mini">接口文档</el-button>
+      </div>
+    </div>
     <div class="g-interface-content">
       <interface-config @update-success="getOneInterface"></interface-config>
       <scene-manage @update-success="getOneInterface"></scene-manage>
@@ -29,6 +43,7 @@ export default {
   },
   data() {
     return {
+      enabled: false,
       projectName: window.context.projectName || ''
     }
   },
@@ -56,6 +71,25 @@ export default {
 .g-interface-detail {
   height: inherit;
   padding: 0 20px;
+  .g-interface-header {
+    position: relative;
+    .m-btngroup {
+      position: absolute;
+      top: 5px;
+      right: 0;
+      .u-switch {
+        display: inline-block;
+        padding: 0 15px;
+        margin-right: 5px;
+        line-height: 28px;
+        vertical-align: -1px;
+        background-color: #409EFF;
+        border-color: #409EFF;
+        border-radius: 3px;
+        color: #fff;
+      }
+    }
+  }
   .g-interface-content {
     box-sizing: border-box;
     height: calc(100% - 40px);
